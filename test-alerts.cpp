8e7514f7477be68806e00cd0c9143cafc1e01b9f
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-05-22 23:31:03 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -11,6 +11,25 @@ TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach( 31, 0, 30) == TOO_HIGH);
   REQUIRE(inferBreach( 46, 0, 45) == TOO_HIGH);
   REQUIRE(inferBreach( 41, 0, 40) == TOO_HIGH);
-  REQUIRE(inferBreach( 29, 0, 30) == NORMAL);
-  
+  REQUIRE(inferBreach( 29, 0, 30) == NORMAL);  
+}
+
+TEST_CASE("Classify the temperature breach based on cooling type")
+{
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,-1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING,31) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,-1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING,45) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,-1) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,0) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING,41) == TOO_HIGH);
+}
+
+TEST_CASE("Alerter")
+{
+  BreachType breachType;
+  breachType.coolingType=PASSIVE_COOLING;
+  REQUIRE(checkAndAlert(TO_EMAIL,"New",50,"abc@xyz.com") == "Temperature is too high");
 }
